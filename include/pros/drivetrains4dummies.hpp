1bf08d1bd60a2f69b7874c6c07a206b9c958369b
Hepodon <brandonlewis0120@gmail.com>
2025-05-12 21:13:28 -0500

    smh

--- include/pros/drivetrains4dummies.hpp
+++ include/pros/drivetrains4dummies.hpp
@@ -31,7 +31,6 @@ public:
   void set_driveVelocity(int driveVelocity) { _driveVelocity = driveVelocity; }
   void set_turnVelocity(int turnVelocity) { _turnVelocity = turnVelocity; }
 
-private:
   int _wheelbase;
   int _wheelDiameter;
   int _gearRatio;
@@ -42,12 +41,16 @@ private:
 class Drivetrain4Dummies {
 
 public:
-  Drivetrain4Dummies(MotorGroup &_leftMotors, MotorGroup &_rightMotors,
+  Drivetrain4Dummies(MotorGroup &_leftMotors, int leftDistance,
+                     MotorGroup &_rightMotors, int rightDistance,
                      DTSettings &_settings, Imu &_inertial);
   MotorGroup &_leftMotors;
   MotorGroup &_rightMotors;
   Imu &_inertial;
   DTSettings &_settings;
+
+  int _leftDistance;
+  int _rightDistance;
 
   int _x;
   int _y;
@@ -99,6 +102,50 @@ public:
     _settings.set_turnVelocity(turnVelocity);
   }
 
+  void stop() {
+    _leftMotors.brake();
+    _leftMotors.brake();
+  }
+};
+class Drivetrain4DummiesNT {
+
+public:
+  Drivetrain4DummiesNT(MotorGroup &_leftMotors, MotorGroup &_rightMotors,
+                       DTSettings &_settings, Imu &_inertial,
+                       Controller &_controller);
+  MotorGroup &_leftMotors;
+  MotorGroup &_rightMotors;
+  Imu &_inertial;
+  DTSettings &_settings;
+  Controller &_controller;
+
+  void CALIBRATE(int x = 0, int y = 0, int theta = 0) {
+    _inertial.reset();
+    while (_inertial.is_calibrating()) {
+      pros::delay(20);
+    }
+    _leftMotors.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    _rightMotors.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+  }
+
+  int get_Wheelbase() const { return _settings.get_Wheelbase(); }
+  int get_WheelDiameter() const { return _settings.get_WheelDiameter(); }
+  int get_GearRatio() const { return _settings.get_GearRatio(); }
+  int get_driveVelocity() const { return _settings.get_driveVelocity(); }
+  int get_turnVelocity() const { return _settings.get_turnVelocity(); }
+
+  void set_Wheelbase(int Wheelbase) { _settings.set_Wheelbase(Wheelbase); }
+  void set_WheelDiameter(int WheelDiameter) {
+    _settings.set_WheelDiameter(WheelDiameter);
+  }
+  void set_GearRatio(int GearRatio) { _settings.set_GearRatio(GearRatio); }
+  void set_driveVelocity(int driveVelocity) {
+    _settings.set_driveVelocity(driveVelocity);
+  }
+  void set_turnVelocity(int turnVelocity) {
+    _settings.set_turnVelocity(turnVelocity);
+  }
+
   void drive(int velocity) {
     _leftMotors.move(velocity);
     _rightMotors.move(velocity);
@@ -108,8 +155,6 @@ public:
     _leftMotors.brake();
     _leftMotors.brake();
   }
-
-private:
 };
 } // namespace v5
 } // namespace pros
